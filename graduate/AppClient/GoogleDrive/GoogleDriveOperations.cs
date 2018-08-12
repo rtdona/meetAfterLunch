8db4ea0e14a3d1bedf8a62e328b8b3932e4a9831
Lee Min Jin <gwtwind85@gmail.com>
2018-08-12 15:55:19 +0900

    GoogleServiceClient의 GetQuotaAsync() 구현 (테스트안됨)

---
+++ graduate/AppClient/GoogleDrive/GoogleDriveOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace graduate.AppClient.GoogleDrive
+{
+    public partial class GoogleServiceClient : IAppClient
+    {
+        public async Task<SpaceQuota> GetQuotaAsync()
+        {
+            var request = service.About.Get();
+            request.Fields = "storageQuota";
+            var response = await request.ExecuteAsync();
+
+            var used = response.StorageQuota.Usage;
+            var alloc = response.StorageQuota.Limit;
+
+            return new SpaceQuota((ulong)used, (ulong)alloc);
+        }
+    }
+}
